a3880a8a6e5657d868dbfbd67a4feae3f44fbfc6
luishecastilho <manocastilho07@gmail.com>
2023-05-19 01:00:46 -0300

    update on feed

---
+++ src/components/FeedFake.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import "./FeedFake.css";
+
+const FeedFake = () =>{
+    return (
+        <div className="FeedFake">
+            {/* lista de noticias igual porem sombras (igual aquele carregamento ghost) */}
+            {/* animação modal de login ou registro */}
+        </div>
+    )
+}
+export default FeedFake
--- src/routes/Feed.jsx
+++ src/routes/Feed.jsx
@@ -5,6 +5,19 @@ import './Feed.css';
 import GetCookie from '../hooks/GetCookie';
 
 import FeedArticle from "../components/FeedArticle";
+import FeedFake from "../components/FeedFake";
+
+function renderContent() {
+    if(GetCookie('auth_token')){
+        articles.map(function(article) {
+            return (
+                <FeedArticle article={article} key={article.id} />
+            )
+        })
+    }else{
+        <FeedFake />
+    }
+}
 
 function Feed() {
 
@@ -12,19 +25,21 @@ function Feed() {
     const [pagination, setPagination] = useState([]);
 
     useEffect(() => {
-        axios.get('http://127.0.0.1:8000/api/feed', {
-            headers: { 
-                        'Authorization': `Bearer ${GetCookie('auth_token')}`,
-                        'Accept': 'application/json'
-                    }
-        })
-        .then((res) => {
-            setPagination(res.data.data.articles);
-            setArticles(res.data.data.articles.data);
-        })
-        .catch((error) => {
-          console.error(error)
-        })
+        if(GetCookie('auth_token')){
+            axios.get('http://127.0.0.1:8000/api/feed', {
+                headers: { 
+                            'Authorization': `Bearer ${GetCookie('auth_token')}`,
+                            'Accept': 'application/json'
+                        }
+            })
+            .then((res) => {
+                setPagination(res.data.data.articles);
+                setArticles(res.data.data.articles.data);
+            })
+            .catch((error) => {
+            console.error(error)
+            })
+        }
     }, []);
 
   return (
